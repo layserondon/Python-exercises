97138fcfaf280614340a6fd584c5972e8d56e019
Layse <layserondon@gmail.com>
2026-06-12 19:43:24 -0300

    Últimos erros consertados

--- lists.py
+++ lists.py
@@ -1,71 +1,83 @@
 #Gerenciamento de Pedidos de uma empresa
 
+atendimentos = []
+atendimentos_outro = ['Thor - Shih-tzu', 'Amelia - Persa', 'Ônix - Husky', 'Violeta - Golden']
+
 while True:
     print(
-        """ 1 - Cadastrar Cliente
+        """ 1 - Cadastrar Cliente 
             2 - Inserir Novo Atendimento
             3 - Importar Lista de Atendimento
             4 - Cancelar Atendimento
-            5 - Concluir Atendimento
-            6 - Listar Atendimentos Pendentes
-            7 - Ordenar nomes
-            8 - Procurar Atendimento
-            9 - Mostrar Valor Total do Serviço
-            10 - Mostrar Serivço Mais Caro 
-            11 - Mostrar Serviço Mais Barato
-            12 - Exportar Clientes
-            13 - Importar Novos Clientes
-            14 - Inverter Ordem de Atendimento 
-            0 - Sair
-            """ 
-        )
-    atendimentos = []
-    atendimentos_outro = ['Thor - Shih-tzu', 'Amelia - Persa', 'Ônix - Husky', 'Violeta - Golden']
+            5 - Concluir Atendimento""")
+        #     6 - Listar Atendimentos Pendentes
+        #     7 - Ordenar nomes
+        #     8 - Procurar Atendimento
+        #     9 - Mostrar Valor Total do Serviço
+        #     10 - Mostrar Serivço Mais Caro 
+        #     11 - Mostrar Serviço Mais Barato
+        #     12 - Exportar Clientes
+        #     13 - Importar Novos Clientes
+        #     14 - Inverter Ordem de Atendimento 
+        #     0 - Sair
+        #     """ 
+        # )
 
     escolha = int(input("Como podemos te ajudar? "))
 
     if escolha == 1:
+
         tutor = input("Nome do Responsável: ")
         pet = input("Nome do pet: ")
         raca = input("Raça do pet: ")
-        servico = input("Serviços: [1-Banho, 2-Hidratação, 3-Tosa Hig., 4-Tosa da Raça, 5-Tosa Bebê, 6-Escovação]")
-        novo_cadastro = print(tutor, pet, raca, servico)
+        servico = input("Serviços: [Banho, Hidratação, Tosa Hig., Tosa da Raça, Tosa Bebê, Escovação]")
+        novo_cadastro = f"{tutor} - {pet} - {raca} - {servico}"
         atendimentos.append(novo_cadastro)
         print(atendimentos)
+
     elif escolha == 2 :
+
         tutor = input("Nome do Tutor: ")
         pet = input("Nome do Pet: ")
-        servico = input("Serviços: [1-Banho, 2-Hidratação, 3-Tosa Hig., 4-Tosa da Raça, 5-Tosa Bebê, 6-Escovação]")
-        novo_atendimento = print(tutor, pet, servico)
+        servico = input("Serviços: [1-Banho, 2-Hidratação, 3-Tosa Hig., 4-Tosa da Raça, 5-Tosa Bebê, 6-Escovação]") 
+        novo_atendimento = f"{tutor} - {pet} - {servico}"
         atendimentos.insert(-1, novo_atendimento)
         print(atendimentos)
+
     elif escolha == 3:
+
         atendimentos.extend(atendimentos_outro)
-        print("Importação realizada com sucesso!")
         print(atendimentos)
+
     elif escolha == 4:
-        for index, atendimentos in enumerate(atendimentos, start=1):
-            print(index, atendimentos)
-        cancelar_atend = input("Qual atendimento você quer cancelar? ")
-        try:
-            numero = int(cancelar_atend)
-            atendimentos.remove(numero)
-        except:
-            print("Número inválido. [Atenção] A ação não foi concluída!")
-            break
+        if len(atendimentos) == 0:
+            print("não há atendimentos para cancelar.")
+        else:
+            for index, atendimento in enumerate(atendimentos, start=1):
+                print(index, atendimento)
+            
+            cancelar_atend = int(input("Qual atendimento você quer cancelar? "))
+        
+            if cancelar_atend >= 1 and cancelar_atend <= len(atendimentos):
+                indice_real = cancelar_atend - 1
+                atendimentos.pop(indice_real)
+                print(f"Atendimento cancelado com sucesso!: {atendimentos}")
+            else:
+                print("Número inválido. [Atenção] A ação não foi concluída!")
+                break
+
     elif escolha == 5:
-        # Concluir Atendimento
-        for index, atendimentos in enumerate(atendimentos, start=1):
-            print(index, atendimentos)
-        concluir = input("Qual atendimento foi concluído? ")
-        try:
-            numero = int(concluir)
-            indice = numero - 1
-            atendimentos.pop(indice)
-        except:
-            print("Número inválido. [Atenção] A ação não foi concluída!")
-            break
+        if len(atendimentos) == 0:
+            print("Não há atendimentos para concluir!")
+        else:
+            for index, atendimento in enumerate(atendimentos, start=1):
+                print(index, atendimento)
+
+                concluir = int(input("Qual atendimento foi concluído? "))
+
+            if concluir >= 1 and concluir <= len(atendimentos):
+                indice_real = concluir - 1
+                atendimentos.pop(indice_real)
+                print(f"Atendimento realizado com sucesso! {atendimentos}")
     else:
         break
-
-
